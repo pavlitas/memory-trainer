5f6eda72169254729030703a3a1b156f33cb8acd
momyno <31783515+pavlitas@users.noreply.github.com>
2026-02-15 20:21:30 +0100

    memory-trainer-1.1

--- js/GiocoMemoria.js
+++ js/GiocoMemoria.js
@@ -6,6 +6,10 @@ class GiocoMemoria {
         this.numeriDisponibili = [];
         this.numeroCorrente = null;
 
+        this.iniziotimer = null;
+        this.tempoTotale = 0;
+        this.tempoTrascorso = 0;   
+
         // Aggiungi le cifre singole: "0", "1", "2"... "9"
         for (let i = 0; i <= 9; i++) {
             this.numeriDisponibili.push(i.toString());
@@ -20,14 +24,12 @@ class GiocoMemoria {
         this.mescolaArray(this.numeriDisponibili);
     }
 
-
     mescolaArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]]; // Scambio
         }
     }
-    
 
     generaNumeroCasuale() {
         // Se l'array è vuoto, ho finito
@@ -35,12 +37,18 @@ class GiocoMemoria {
             return null;
         }
         
+        this.inizioTimer = Date.now();
+
         // Prendo l'ultimo numero e lo rimuovo dall'array
         this.numeroCorrente = this.numeriDisponibili.pop();
         return this.numeroCorrente;
     }
 
     verificaRisposta(rispostaUtente) {
+        const fineTimer = Date.now();
+        this.tempoTrascorso = fineTimer - this.inizioTimer;
+        this.tempoTotale += this.tempoTrascorso;
+
         // 1. Ottieni la risposta corretta dallo schedario
         const rispostaCorretta = this.schedario.getImmagine(this.numeroCorrente);
         
@@ -51,14 +59,56 @@ class GiocoMemoria {
             // 4. Ritorna un oggetto con il risultato
             return {
                 corretta: true,
-                rispostaCorretta: rispostaCorretta
+                rispostaCorretta: rispostaCorretta,
+                immagine: this.schedario.getPercorsoImmagine(this.numeroCorrente),
+                tempoTrascorso: this.tempoTrascorso
             };
         } else {
             // Se sbagliata
             return {
                 corretta: false,
-                rispostaCorretta: rispostaCorretta
+                rispostaCorretta: rispostaCorretta,
+                immagine: this.schedario.getPercorsoImmagine(this.numeroCorrente),
+                tempoTrascorso: this.tempoTrascorso
             };
         }
+        
+        
+    }
+
+    salvaProgressi() {
+        // Cosa dobbiamo salvare?
+        // 1. Punteggio
+        // 2. Tempo totale
+        // 3. Numeri disponibili (quelli non ancora fatti)
+        
+        const dati = {
+            punteggio: this.punteggio,
+            tempoTotale: this.tempoTotale,
+            numeriDisponibili: this.numeriDisponibili
+        };
+        
+        // Converti in stringa e salva
+        localStorage.setItem('memoryTrainerDati', JSON.stringify(dati));
+    }
+
+    caricaProgressi() {
+        // Prendi i dati salvati
+        const datiSalvati = localStorage.getItem('memoryTrainerDati');
+        
+        // Se non ci sono dati salvati, esci
+        if (!datiSalvati) {
+            return false;  // Nessun salvataggio trovato
+        }
+        
+        // Converti da stringa a oggetto
+        const dati = JSON.parse(datiSalvati);
+        
+        // Ripristina i valori
+        this.punteggio = dati.punteggio;
+        this.tempoTotale = dati.tempoTotale;
+        this.numeriDisponibili = dati.numeriDisponibili;
+        
+        return true;  // Caricamento riuscito
     }
 }
